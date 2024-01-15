d7123311f9d9ba31825a4a69afdecbe34c6ab02d
Caleb Chang <calebschang1204@gmail.com>
2024-01-15 13:09:58 -0800

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,12 @@
+const myFamily = 4;
+const myHouse = 7;
+const myFood = 10 + 6;
+const myWater = 3 * 2;
+const mySpend = 6;
+const myWaste = 30 + 5;
+const myRecycle = 24 - (4*4);
+const myTravel = 12 + 2 + 6;
+
+const myTotal = myFamily + myHouse + myFood + myWater + mySpend + myWaste + myRecycle + myTravel; 
+const element = document.querySelector("h2");
+element.innerHTML = myTotal;
